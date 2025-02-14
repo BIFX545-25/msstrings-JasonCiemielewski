307a70625d366cca32caf26289009756dafc1948
JasonCiemielewski <ciemielewski.jason@gmail.com>
2025-02-13 20:03:40 -0500

    Documentation, testing, additional updates

---
+++ R/dataDoc.R
@@ -0,0 +1,9 @@
+#' Tittle
+#'
+#' Fragments with protein modifications that we might expect to find
+#' from the CCR5 gene in a mass spec experiment.
+#'
+#' @format  Character vector of length 4 with peptide fragments
+#'
+#' @source data_raw/data.R
+"MSfrags"
--- R/parseMSstrings.R
+++ R/parseMSstrings.R
@@ -1,3 +1,14 @@
+#' pareseMSstrings.R
+#' Extracts metadata from annotated peptide sequence from Spectronaut, returns tibble with sequence, modifications, and modification locations in peptide sequence.
+#'
+#' @param seqs A character string. Sequence represents amino acid sequence
+#' @param mods A character string. Sequence represents modifications to amino acids
+#' @param mods_at A numeric value. Location of modifications in sequence
+#'
+#' @returns A tibble.
+#' @export
+#' @importFrom dplyr purrr Biostrings stringr str_extract_all str_replace_all
+
 # parseMSstrings.R
 # utilities for parsing MS strings
 
@@ -11,8 +22,8 @@ parse_mods <- function(seqs, format = 'Spectronaut')
   # remove '_'
   seqs <- seqs |>
     str_replace_all('_', '')
-  
-  
+
+
   # locate modification(s)
   mods <- str_extract_all(seqs, "\\[.*?\\]") |>
     map(~ .x |>
@@ -20,30 +31,30 @@ parse_mods <- function(seqs, format = 'Spectronaut')
           str_replace(fixed("]"), '') |>
           str_replace("\\(.*?\\)", '') |>
           trimws())
-  
-  
+
+
   # remove modifications from seqs
   seqs <- str_split(seqs, "\\[.*?\\]")
-  
-  
+
+
   # find where the mods are at
-  mods_at <- map(seqs, ~ 
+  mods_at <- map(seqs, ~
                    {
                      retval <- nchar(.x) |>
                        cumsum()
-                     
+
                      retval[-length(retval)] # this is the end of the peptide, not a modification
                    })
-  
-  
+
+
   # return the data.frame
   retval <- tibble(sequence = map_chr(seqs, ~ paste(.x, collapse = '')) |>
                      AAStringSet() |>
                      as.list(),
                    mods = mods,
                    mods_at = mods_at)
-  
+
   class(retval) <- c('protein_mods', class(retval))
-  
+
   return(retval)
 }
